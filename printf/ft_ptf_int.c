5c439c394e6daf2fe32f5f97c9f0f414e0df4c86
kiezmor <vpluchar@student.42.fr>
2017-09-15 16:25:05 +0200

    debug 5

--- printf/ft_ptf_int.c
+++ printf/ft_ptf_int.c
@@ -73,7 +73,7 @@ int							ft_ptf_int(va_list arg)
 
 	flags = get_ptf_flags(GET_FLAGS);
 	nbr = get_arg(arg, flags->length);
-	ft_pft_putnbr(get_nbr(nbr), 10, flags->conversion);
+	ft_ptf_putnbr(get_nbr(nbr), 10, flags->conversion);
 	int_rules(flags);
 	write_int(nbr, flags);
 	return (0);
