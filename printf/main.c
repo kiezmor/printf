4d22b7af98441796c9e0cc1378e0c17bbae0c84e
kiezmor <vpluchar@student.42.fr>
2017-09-28 13:55:22 +0200

    well try 02

--- printf/main.c
+++ printf/main.c
@@ -76,6 +76,7 @@ int	main(void)
 	// ft_itoa(n,tab, 10);
 	// printf("%s\n", tab);
 	printf("%d\n", tab);
+	printf("%c\n", (char)n);
 
 	printf("%s\n", "------------------------------------------------");
 
