1025db812a7bb2354e581f6ca6e4240b06f2453c
kiezmor <vpluchar@student.42.fr>
2017-10-01 23:17:43 +0200

    ptf or not 28

--- m.c
+++ m.c
@@ -52,8 +52,10 @@ int	main(void)
 	wchar_t b = L'\u03c6';
 
 	ft_printf("[%C]\n", wc);
+	ft_printf("{%C}\n", a);
 	printf("%lc\n", a);
 	ft_printf("[|%C|]\n", wch);
+	ft_printf("{|%C|}\n", b);
 	printf("%lc\n", b);
 
 	// printf("%s\n", "------------------------------------------------");
