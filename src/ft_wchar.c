b56a86bf336b689baf122a2d4a307a43bf10e253
kiezmor <vpluchar@student.42.fr>
2017-10-08 15:36:18 +0200

    last patch? 05

--- src/ft_wchar.c
+++ src/ft_wchar.c
@@ -53,68 +53,45 @@ static int		write_4byte_char(int c, char *tab)
 	return (ft_ptf_buff(tab, BUF_WRITE));
 }
 
-// static int		write_2byte_char(int c, char *tab)
-// {
-// 	int	len;
+static int		write_2byte_char(int c, char *tab)
+{
+	int	len;
 
-// 	len = 0;
-// 	if (c < 0x80)
-// 		tab[len++] = c;
-// 	else if (c < 0x800)
-// 	{
-// 		tab[len++] = 0xC0 | (c >> 6);
-// 		tab[len++] = 0x80 | (c & 0x3F);
-// 	}
-// 	else if (c < 0x10000)
-// 	{
-// 		tab[len++] = 0xE0 | (c >> 12);
-// 		tab[len++] = 0x80 | ((c >> 6) & 0x3F);
-// 		tab[len++] = 0x80 | (c & 0x3F);
-// 	}
-// 	tab[len] = 0;
-// 	if (!tab[0])
-// 	{
-// 		printf("%s|buff2|\n", tab);
-// 		return (ft_ptf_buff(tab, BUF_CHAR));
-// 	}
-// 	else
-// 	{
-// 		printf("%s|buff3|\n", tab);
-// 		return (ft_ptf_buff(tab, BUF_WRITE));
-// 	}
-// }
+	len = 0;
+	if (c < 0x80)
+		tab[len++] = c;
+	else if (c < 0x800)
+	{
+		tab[len++] = 0xC0 | (c >> 6);
+		tab[len++] = 0x80 | (c & 0x3F);
+	}
+	else if (c < 0x10000)
+	{
+		tab[len++] = 0xE0 | (c >> 12);
+		tab[len++] = 0x80 | ((c >> 6) & 0x3F);
+		tab[len++] = 0x80 | (c & 0x3F);
+	}
+	tab[len] = 0;
+	if (!tab[0])
+	{
+		// printf("%s|buff2|\n", tab);
+		return (ft_ptf_buff(tab, BUF_CHAR));
+	}
+	else
+	{
+		// printf("%s|buff3|\n", tab);
+		return (ft_ptf_buff(tab, BUF_WRITE));
+	}
+}
 
 int			ft_ptf_wchar(int c)
 {
-	// char	tab[10];
-
-	// printf("%d|int c|\n", c);
-
-	// if (c < 0x200000)
-	// 	return (write_2byte_char(c, tab));
-	// else
-	// 	return (write_4byte_char(c, tab));
-	// char	t[4];
-	char	t[4];
 	char	tab[10];
 
+	printf("%d|int c|\n", c);
+
 	if (c < 0x200000)
-	{
-		if (c >= 32 && c <= 126)
-		{
-			t[0] = (char)c;
-			return(ft_ptf_buff(t, BUF_CHAR));
-		}
-		ft_wctomb(t, (wchar_t)c);
-		return(ft_ptf_buff(t, BUF_WRITE));
-	}
-	else	
+		return (write_2byte_char(c, tab));
+	else
 		return (write_4byte_char(c, tab));
-	// if (c >= 32 && c <= 126)
-	// {
-	// 	t[0] = (char)c;
-	// 	return(ft_ptf_buff(t, BUF_CHAR));
-	// }
-	// ft_wctomb(t, (wchar_t)c);
-	// return(ft_ptf_buff(t, BUF_WRITE));
 }
